f54cbbb77afba560e95c6f8a4118322d88c4880e
nvnavaneeth <nvnava123@gmail.com>
2019-04-04 20:42:34 +0530

    Basic structure

---
+++ page_replacement_algos.cpp
@@ -0,0 +1,21 @@
+#include"page_replacement_algos.h"
+
+#include<iostream>
+
+using namespace std;
+
+shared_ptr<ReadPageResult> PageManagerLRUCounter::ReadPage(int page_no) {
+  cout<<"ERR: To be implemented";
+ }
+
+shared_ptr<ReadPageResult> PageManagerLRUStack::ReadPage(int page_no) {
+  cout<<"ERR: To be implemented";
+ }
+
+shared_ptr<ReadPageResult> PageManagerLRUAging::ReadPage(int page_no) {
+  cout<<"ERR: To be implemented";
+ }
+
+shared_ptr<ReadPageResult> PageManagerLRUClock::ReadPage(int page_no) {
+  cout<<"ERR: To be implemented";
+ }
